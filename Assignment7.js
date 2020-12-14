3c9d600c067b45acd7bdf40ca53c83e06d76b370
Minhchau0108 <74655110+Minhchau0108@users.noreply.github.com>
2020-12-14 16:18:49 +0700

    Update Assignment7.js

--- Assignment7.js
+++ Assignment7.js
@@ -1,4 +1,4 @@
 //Print out a random number between 0 and 1
-console.log(Math.floor(Math.random()));
+console.log(Math.floor(Math.random()*2));
 //Print out a random number between 0 and 10
 console.log(Math.floor(Math.random() * 10));
